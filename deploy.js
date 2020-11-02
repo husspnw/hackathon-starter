f22ea87f119a38fa3f0d141116c91d45ac3f78b4
husspnw <73805535+husspnw@users.noreply.github.com>
2020-11-01 18:13:24 -0800

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '52.90.158.140',
+      host: '107.21.171.20',
       username: 'ubuntu',
       privateKey: 'hs-key.pem'
     })
